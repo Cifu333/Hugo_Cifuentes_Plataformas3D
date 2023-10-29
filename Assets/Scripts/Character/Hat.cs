db571905d69e58941061abe62ddb218b7421a3da
hugo cifuentes <hugo.cifuentes@enti.cat>
2023-10-29 19:45:38 +0100

    lanzar sombrero

--- Assets/Scripts/Character/Hat.cs
+++ Assets/Scripts/Character/Hat.cs
@@ -4,12 +4,43 @@ using UnityEngine;
 
 public class Hat : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject sombrero;
+
+    [SerializeField] GameObject target;
+
+    private float timer = 3f;
+
+    private bool isCreate = false;
 
     void Update()
     {
-        if (Input_Manager._INPUT_MANAGER.GetTrowHat())
+        if (Input_Manager._INPUT_MANAGER.GetTrowHat() && isCreate !=true )
         {
+           isCreate = true;
+            sombrero.transform.position = target.transform.position;
+            timer = 3f;
             Debug.Log("GGGGGGGGGGGGGGGGGGGG");
         }
+
+        
+        if(isCreate)
+        {
+            sombrero.SetActive(true);
+            timer -= Time.deltaTime;
+            if(timer <= 0)
+            {
+                isCreate=false;
+                sombrero.SetActive(false);
+            }
+        }
+        else
+        {
+            sombrero.transform.position = target.transform.position;
+        }
+
+
+
     }
+
 }
