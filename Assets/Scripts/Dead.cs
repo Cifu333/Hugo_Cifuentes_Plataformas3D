4f09f3475d7d7f4957e24e59a190e2929cf20af4
hugo cifuentes <hugo.cifuentes@enti.cat>
2023-10-29 22:17:43 +0100

    muerte recargar escena

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Dead : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class Dead : MonoBehaviour
     {
         if(other.gameObject == player)
         {
-            Destroy(player);
+            SceneManager.LoadScene("SampleScene");
 
             Debug.Log("Game Over");
         }
